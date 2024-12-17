c1c5ad3d167d0488c2796b85d42a1122c638ea6f
yassir56069 <hoossanbukshyassir@gmail.com>
2024-12-17 03:23:32 -0800

    🧵Working Pages and Completed DynamicTables

---
+++ Models/CustomerDeviceDto.cs
@@ -0,0 +1,14 @@
+﻿namespace JKPlaystore.Models
+{
+    public class CustomerDeviceDto
+    {
+        public int DeviceID { get; set; }
+        public string? DeviceCode { get; set; }
+        public string? DeviceModel { get; set; }
+        public string? CustomerKey { get; set; }
+        public DateTime TokenInitDate { get; set; }
+        public string? TokenValue { get; set; }
+        public DateTime? TokenExpiry { get; set; }
+    }
+
+}
